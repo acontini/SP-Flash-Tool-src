2be13aba82e3f806c537aa7c1b1cf730304dc164
Davide Guidi <davide@planetcom.co.uk>
2018-06-14 15:36:36 +0100

    Partition download fix

--- Cmd/DLOnlyCommand.cpp
+++ Cmd/DLOnlyCommand.cpp
@@ -33,8 +33,6 @@ DLOnlyCommand::~DLOnlyCommand()
 
 bool DLOnlyCommand::CheckPMTLayoutChange(const QSharedPointer<Connection> &conn)
 {
-    bool ret = false;
-
     APCore::CoreHandle *core_handle = __POOL_OF_HANDLE(key_);
     if(core_handle == NULL)
     {
@@ -70,18 +68,27 @@ bool DLOnlyCommand::CheckPMTLayoutChange(const QSharedPointer<Connection> &conn)
         const PART_INFO *pmt =
                 proxy.read_pmt(rom_it->name);
 
+	bool partitionSelected = false;
         if (tw) {
             QTableWidgetItem *twi = tw->item(row, 0);
             if (twi) {
-                qDebug() << "name" << rom_it->name << twi->checkState();
+                if (pmt)
+                    qDebug() << "Partition Name" << QString::fromLatin1(rom_it->name) << twi->checkState() << QString::fromLatin1(pmt->name);
+                else
+                    qDebug() << "Partition Name" << rom_it->name << twi->checkState() << "MISSING";
+
                 if (twi->checkState() != 0 && warningWrongFlash)
                     return true;
+
+                if (!pmt)
+                    warningWrongFlash = true;
+
+                if (twi->checkState())
+                    partitionSelected = true;
             }
             else
-                qDebug() << "twi" << twi;
+                qDebug() << "Done";
         }
-        else
-            qDebug() << "name" << rom_it->name << "TW" << tw;
 
         if (rom_it->item_is_visable)
             row++;
@@ -100,13 +107,17 @@ bool DLOnlyCommand::CheckPMTLayoutChange(const QSharedPointer<Connection> &conn)
                          rom_it->name, pmt->begin_addr, rom_it->begin_addr, pmt->image_length, rom_it->partition_size);
                 LOGD("PMT changed for <%s>: addr<0x%llx>-->addr<0x%llx>, len<0x%llx>-->len<0x%llx>",
                          rom_it->name, pmt->begin_addr, rom_it->begin_addr, pmt->image_length, rom_it->partition_size);
-                warningWrongFlash = true;
+
+                if (pmt->begin_addr != rom_it->begin_addr && partitionSelected) {
+                    qDebug() << "Starting offset differs - cannot proceed";
+                    return true;
+                }
             }
         }
 
     }
 
-    return ret;
+    return false;
 }
 
 void DLOnlyCommand::exec(const QSharedPointer<Connection> &conn)
